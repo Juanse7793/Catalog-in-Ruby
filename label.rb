158cca8ef203017be8b475310461ce23a48a89c3
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-27 23:58:00 -0500

    label properties added

--- label.rb
+++ label.rb
@@ -1,4 +1,12 @@
 require_relative 'item'
 
 class Label
+
+    def initialize (title, color)
+        id = Random.rand(1..100)
+        @title = title
+        @color = color
+        @items = []
+    end
+
 end
