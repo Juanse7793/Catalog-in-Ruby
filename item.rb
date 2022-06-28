a1824e4ba58b490a0b6d2a6444cb35c250fca340
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-27 22:59:22 -0500

    item class properties created

--- item.rb
+++ item.rb
@@ -1,2 +1,14 @@
 class Item
+    attr_accessor :publish_date
+    attr_reader :genre, :author, :label, :source
+
+    def initialize(genre, author, source, label, publish_date)
+        @id = Random.rand(1..1000)
+        @author = author
+        @genre = genre
+        @label = label
+        @source = source
+        @publish_date = publish_date
+        @archived = false
+    end
 end
