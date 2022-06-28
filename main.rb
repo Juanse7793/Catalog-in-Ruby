efa06eb47d8b872679f410d94004324d3dd68b38
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-06-27 23:31:53 -0500

    linters

--- main.rb
+++ main.rb
@@ -1,47 +1,47 @@
 require_relative 'item'
 
 class Main
-    def main_menu
-        puts "Welcome to the Catalog of Things!"
-        puts "Please select an option:"
-        puts "1. List all books"
-        puts "2. List all music albums"
-        puts "3. List all games"
-        puts "4. List all genres"
-        puts "5. List all labels"
-        puts "6. List all authors"
-        puts "7. Add a book"
-        puts "8. Add a music album"
-        puts "9. Add a game"
-        puts "10. Exit"
-        puts "Enter your selection: "
-        selection = gets.chomp.to_i
-        case selection
-        when 1
-            list_books
-        when 2
-            list_music_albums
-        when 3
-            list_games
-        when 4
-            list_genres
-        when 5
-            list_labels
-        when 6
-            list_authors
-        when 7
-            add_book
-        when 8
-            add_music_album
-        when 9
-            add_game
-        when 10
-            exit
-        else
-            puts "Invalid selection. Please try again."
-            main_menu
-        end
+  def main_menu # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity
+    puts 'Welcome to the Catalog of Things!'
+    puts 'Please select an option:'
+    puts '1. List all books'
+    puts '2. List all music albums'
+    puts '3. List all games'
+    puts '4. List all genres'
+    puts '5. List all labels'
+    puts '6. List all authors'
+    puts '7. Add a book'
+    puts '8. Add a music album'
+    puts '9. Add a game'
+    puts '10. Exit'
+    puts 'Enter your selection: '
+    selection = gets.chomp.to_i
+    case selection
+    when 1
+      list_books
+    when 2
+      list_music_albums
+    when 3
+      list_games
+    when 4
+      list_genres
+    when 5
+      list_labels
+    when 6
+      list_authors
+    when 7
+      add_book
+    when 8
+      add_music_album
+    when 9
+      add_game
+    when 10
+      exit
+    else
+      puts 'Invalid selection. Please try again.'
+      main_menu
     end
+  end
 end
 
 Main.new.main_menu
